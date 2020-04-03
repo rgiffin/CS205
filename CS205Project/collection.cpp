548540443fcf3745e1e9e8363fb2d56040490769
ryanmurf9 <ryanmurf9@gmail.com>
2020-04-03 18:47:34 -0400

    Update collection.cpp

--- CS205Project/collection.cpp
+++ CS205Project/collection.cpp
@@ -1,6 +1,21 @@
 #include "collection.h"
-
+using namespace std;
 Collection::Collection()
 {
 
+}
+
+void Collection::createTable()
+{
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("TestBase");
+    QSqlQuery query;
+    query.exec("create table cTable (id string name, description varchar(200), artist varchar(30))");
+}
+
+bool Collection::addItem(string x, string y, string z)
+{
+    QSqlQuery query;
+     query.exec("insert into cTable values('Mona Lisa', 'A Painting', 'da Vinci')");
+     return true;
 }
