dbc1a30ab9bd6250d3534244d389b45ffc4d511b
ryanmurf9 <ryanmurf9@gmail.com>
2020-04-03 18:47:38 -0400

    Update main.cpp

--- CS205Project/main.cpp
+++ CS205Project/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
+#include "collection.h"
 
 using namespace std;
 
 int main()
 {
+    Collection c;
+    c.createTable();
+    c.addItem("Mona Lisa", "A Painting", "da Vinci");
+
     cout << "Hello World!" << endl;
-
-
-
     cout << "Hello Ryan" << endl;
-
-
-
     return 0;
 }
