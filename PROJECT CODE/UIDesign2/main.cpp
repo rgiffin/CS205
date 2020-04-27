f0b5c20668a019e9c4f9d070515740d1dd76df0b
ryanmurf9 <ryanmurf9@gmail.com>
2020-04-27 15:36:39 -0400

    created new museum table and made add item function

--- PROJECT CODE/UIDesign2/main.cpp
+++ PROJECT CODE/UIDesign2/main.cpp
@@ -17,9 +17,10 @@ void openDatabases()
         cout << "DATABASE COULD NOT BE OPENED" << endl;
     }
     QSqlQuery query;
-    query.exec("create table itemTable (name varchar(100), description varchar(200), artist varchar(30))");
-    query.exec("create table userTable(user varchar(50), pass varchar(50), name varchar(50), email varchar(75), type varchar(10))");
+    query.exec("create table itemTable (name varchar(100), description varchar(500), artist varchar(100), owner varchar(100), museum varchar(100), collection varchar(100))");
+    query.exec("create table userTable (user varchar(50), pass varchar(50), name varchar(50), email varchar(75), type varchar(10))");
     query.exec("create table commentTable (comment varchar(300), user varchar(200), item varchar(100))");
+    query.exec("create table museumTable (museum varchar(300), owner varchar(200))");
 }
 
 int main(int argc, char *argv[])
